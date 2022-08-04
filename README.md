cca0c9582a811a3fec8a499430eb16ecc3ca93f7
Revaldi <revaldii32@gmail.com>
2022-08-04 19:35:26 +0700

    [IMPROVE] - Finish App Development + Deployment using Netlify Heroku

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ This Memories App was created using MERN Stack Application following the tutoria
 PSA, this project is for learning purposes, meaning the code written and library versions used are the exact same as shown in the tutorial mentioned above.
 
 ### REAPZ ###
+
+To look at the hosted app, please visit the following urls.
+
+Frontend (React): https://revaldir-memories-app.netlify.app/
+
+Backend API (Express, MongoDB, Node.js): https://mern-memories-mjs.herokuapp.com/
+
+# API DOCS #
+1. .../posts/ (GET) => Fetch all posts
+2. .../posts/ (POST) => Create new post
+3. .../posts/:id (PATCH) => Update post by _id
+4. .../posts/:id (DELETE) => Delete post by _id
+5. .../posts/:id/likePost (PATCH) => Increase posts like count by _id
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -13,13 +15,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Reapz memories API!');
+});
+
 // Connect to MongoDB
-const CONNECTION_URL = 'mongodb+srv://revaldir:revaldi32@memoriesmern.yvuqw.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`)))
     .catch((err) => console.log(err.message));
 
-// mongoose.set('')
-// mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);
